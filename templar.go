f55b6861eb85113dce9f9518eda48161e927d5e7
Martin Bruse <zondolfin@gmail.com>
2014-03-20 13:02:42 +0100

    better working

--- templar.go
+++ templar.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 	"text/template"
 )
@@ -48,12 +49,54 @@ func Add(name, text string) (err error) {
 	return
 }
 
-func Get(name string) (result *template.Template, err error) {
+func GetMatching(diskSearch bool, baseName string, reg string) (result *template.Template, err error) {
+	pat, err := regexp.Compile(reg)
+	if err != nil {
+		return
+	}
+	result = template.New(baseName)
+	if _, err = result.Parse(""); err != nil {
+		return
+	}
+	for name, templar := range templars {
+		if pat.MatchString(name) {
+			if _, err = result.AddParseTree(filepath.Base(name), templar.Tree); err != nil {
+				err = fmt.Errorf("Error adding %#v to result: %v", filepath.Base(name), err)
+				return
+			}
+		}
+	}
+	if len(result.Templates()) > 0 {
+		return
+	}
+	if diskSearch {
+		var allChildren []string
+		allChildren, err = children(".")
+		if err != nil {
+			return
+		}
+		filenames := []string{}
+		for _, child := range allChildren {
+			if pat.MatchString(child) {
+				filenames = append(filenames, child)
+			}
+		}
+		if result, err = result.ParseFiles(filenames...); err != nil {
+			return
+		}
+	}
+	return
+}
+
+func Get(diskSearch bool, name string) (result *template.Template, err error) {
 	result, found := templars[name]
 	if found {
 		return
 	}
-	return template.ParseFiles(name)
+	if diskSearch {
+		result, err = template.ParseFiles(name)
+	}
+	return
 }
 
 func Generate(dir, dst string) (err error) {
@@ -75,7 +118,6 @@ func Generate(dir, dst string) (err error) {
 	}
 	defer dstFile.Close()
 	if _, err = fmt.Fprintf(dstFile, `package %v
-import "text/templates"
 import "github.com/zond/templar"
 func init() {
 `, filepath.Base(filepath.Dir(dst))); err != nil {
